01716489e42d2feb05edd9e761a4092a023b651b
Murat Demirturk <muratdemirturk@yahoo.com>
2020-10-19 21:06:58 -0400

    mike

---
+++ src/main/java/com/homifyin/nameAPI/NameApiApplication.java
@@ -0,0 +1,13 @@
+package com.homifyin.nameAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/homifyin/nameAPI/NameController.java
@@ -0,0 +1,14 @@
+package com.homifyin.nameAPI;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
+public class NameController {
+
+    @GetMapping("/name")
+    public String getName(){
+        return "Mike";
+    }
+}
