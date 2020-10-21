0a1088c21442cc2934473b58029e26c5eec60da6
muratdemirturk <47068914+muratdemirturk@users.noreply.github.com>
2020-10-21 02:54:35 -0400

    Update NameController.java

--- src/main/java/com/homifyin/nameAPI/NameController.java
+++ src/main/java/com/homifyin/nameAPI/NameController.java
@@ -9,6 +9,6 @@ public class NameController {
 
     @GetMapping("/name")
     public String getName(){
-        return "Mike using testin again 7 ";
+        return "Mike using testin again 8 ";
     }
 }
