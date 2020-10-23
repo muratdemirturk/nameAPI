9e30b380022a848af9b3dba92a901dce6b173e1b
muratdemirturk <47068914+muratdemirturk@users.noreply.github.com>
2020-10-22 23:40:49 -0400

    Update NameController.java

--- src/main/java/com/homifyin/nameAPI/NameController.java
+++ src/main/java/com/homifyin/nameAPI/NameController.java
@@ -9,6 +9,6 @@ public class NameController {
 
     @GetMapping("/name")
     public String getName(){
-        return "M.D is going to have ssl soon! ";
+        return "M.D is going to install ssl soon! ";
     }
 }
